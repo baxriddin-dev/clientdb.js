cec0503f5f722cf261198d416dc4c5c8c8adedd2
Baxriddin Chorshanbiyev <baxriddin.developer@gmail.com>
2024-07-04 19:15:31 +0500

    added: create & delete collection methods

---
+++ src/database/database.js
@@ -0,0 +1,52 @@
+import { Storage } from "../utils/storage.js";
+
+export class Database {
+  constructor(databaseName) {
+    const trimmedName = this._validateName(databaseName, 'database');
+
+    this.database = trimmedName;
+    this.collections = Storage.get(this.database) || {};
+  }
+
+  _save() {
+    Storage.set(this.database, this.collections);
+  }
+
+  _validateName(name, type, status = null) {
+    const trimmedName = name?.toString().trim();
+
+    if (!trimmedName && status === 'drop') {
+      throw new Error(`You did not specify the name of the ${type} to delete.`)
+    }
+
+    if (!trimmedName) {
+      throw new Error(`You did not name the ${type}!`);
+    }
+
+    return trimmedName;
+  }
+
+  createCollection(collectionName) {
+    const trimmedName = this._validateName(collectionName, 'collection');
+
+    if (!(trimmedName in this.collections)) {
+      this.collections[trimmedName] = [];
+      this._save();
+
+      return [];
+    } else {
+      return this.collections[trimmedName];
+    }
+  }
+
+  dropCollection(collectionName) {
+    const trimmedName = this._validateName(collectionName, 'collection', 'drop');
+
+    if (trimmedName) {
+      delete this.collections[trimmedName];
+      this._save();
+    } else {
+      console.error(`Collection ${trimmedName} does not exist`)
+    }
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Database } from "./database/database.js";
+import { ID } from "./utils/id.js";
+
+export {
+  Database,
+  ID,
+};
