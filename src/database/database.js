5595f8cec7bd5f97eabe2b206c8ca4de6be0768f
Baxriddin Chorshanbiyev <baxriddin.developer@gmail.com>
2024-07-04 19:34:33 +0500

    added: dropDatabase method

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ import { Storage } from "../utils/storage.js";
 
 export class Database {
   constructor(databaseName) {
-    const trimmedName = this._validateName(databaseName, 'database');
+    const trimmedName = this._validateName(databaseName, "database");
 
     this.database = trimmedName;
     this.collections = Storage.get(this.database) || {};
@@ -15,8 +15,8 @@ export class Database {
   _validateName(name, type, status = null) {
     const trimmedName = name?.toString().trim();
 
-    if (!trimmedName && status === 'drop') {
-      throw new Error(`You did not specify the name of the ${type} to delete.`)
+    if (!trimmedName && status === "drop") {
+      throw new Error(`You did not specify the name of the ${type} to delete.`);
     }
 
     if (!trimmedName) {
@@ -27,7 +27,7 @@ export class Database {
   }
 
   createCollection(collectionName) {
-    const trimmedName = this._validateName(collectionName, 'collection');
+    const trimmedName = this._validateName(collectionName, "collection");
 
     if (!(trimmedName in this.collections)) {
       this.collections[trimmedName] = [];
@@ -40,13 +40,26 @@ export class Database {
   }
 
   dropCollection(collectionName) {
-    const trimmedName = this._validateName(collectionName, 'collection', 'drop');
+    const trimmedName = this._validateName(collectionName, "collection", "drop");
 
     if (trimmedName) {
       delete this.collections[trimmedName];
       this._save();
+
+      return true;
     } else {
-      console.error(`Collection ${trimmedName} does not exist`)
+      console.error(`Collection ${trimmedName} does not exist`);
+      return false;
     }
+  }
+
+  dropDatabase() {
+    Storage.remove(this.database);
+
+    if (!Storage.get(this.database)) {
+      return true;
+    }
+
+    return false;
   }
 }
